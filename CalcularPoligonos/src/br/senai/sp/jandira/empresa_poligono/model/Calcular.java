29195235330eac34a2f537a37b2b1634a04ecbf3
YURI SILVA DE OLIVEIRA <25132703@CFP127ALUNO.LOCAL>
2025-03-10 16:46:55 -0300

    Atividade de Poligonos

---
+++ CalcularPoligonos/src/br/senai/sp/jandira/empresa_poligono/model/Calcular.java
@@ -0,0 +1,17 @@
+package br.senai.sp.jandira.empresa_poligono.model;
+
+import br.senai.sp.jandira.empresa_poligono.Quadrado;
+
+public class Calcular {
+    
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		Quadrado quadrado1 = new Quadrado();
+		quadrado1.setLado(2);
+		quadrado1.exibirDados();
+	}
+
+}
